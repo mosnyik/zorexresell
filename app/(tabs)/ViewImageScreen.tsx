7a08bf191037570c8c8e698c4444c684bf18d16f
Nyikwagh Moses <mosnyik@gmail.com>
2026-04-16 20:58:35 +0100

    feat: created the view image screen and moved the welcome screen to its own file

---
+++ app/(tabs)/ViewImageScreen.tsx
@@ -0,0 +1,51 @@
+import { Image } from "expo-image";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function ViewImageScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topContainer}>
+        <View style={styles.closeTab}></View>
+        <View style={styles.deleteIcon}></View>
+      </View>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../../assets/chair.jpg")}
+          style={styles.image}
+          contentFit="contain"
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  closeTab: {
+    backgroundColor: "tomato",
+    height: 50,
+    width: 50,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  deleteIcon: {
+    backgroundColor: "dodgerblue",
+    height: 50,
+    width: 50,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  imageContainer: {
+    flex: 1,
+    padding: 20,
+  },
+  topContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+});
---
+++ app/(tabs)/WelcomeScreen.tsx
@@ -0,0 +1,69 @@
+import { Image } from "expo-image";
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+function WelcomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        style={styles.background}
+        source={require("../../assets/background.jpg")}
+      >
+        <View style={styles.content}>
+          <Image
+            source={require("../../assets/logo-red.png")}
+            style={styles.logoImage}
+          />
+          <Text style={styles.welcomeText}>
+            Not using it again? Just sell it
+          </Text>
+        </View>
+
+        <View style={styles.bottomContainer}>
+          <View style={styles.signup}></View>
+          <View style={styles.signin}></View>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  bottomContainer: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+  },
+  container: {
+    flex: 1,
+  },
+  content: {
+    alignItems: "center",
+    width: "100%",
+    position: "absolute",
+    top: 60,
+  },
+  logoImage: {
+    height: 120,
+    width: 120,
+  },
+  signup: {
+    backgroundColor: "#fc5c65",
+    height: 50,
+    width: "100%",
+  },
+  signin: {
+    backgroundColor: "#4ecdc4",
+    height: 50,
+    width: "100%",
+  },
+  welcomeText: {
+    marginTop: 20,
+  },
+});
+
+export default WelcomeScreen;
