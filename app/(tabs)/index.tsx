8671ad48de184933d47f1bfbedc1cb9caf13b007
Nyikwagh Moses <mosnyik@gmail.com>
2026-04-16 18:34:05 +0100

    feat: created the home screen with the login and signup buttons placeholder and logo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Image } from "expo-image";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -8,9 +8,21 @@ export default function HomeScreen() {
     <SafeAreaView style={styles.container}>
       <Image
         source={require("../../assets/background.jpg")}
-        style={styles.image}
+        style={styles.background}
         contentFit="cover"
       />
+      <View style={styles.content}>
+        <Image
+          source={require("../../assets/logo-red.png")}
+          style={styles.logoImage}
+        />
+        <Text style={styles.welcomeText}>Not using it again? Just sell it</Text>
+      </View>
+
+      <View style={styles.bottomContainer}>
+        <View style={styles.signup}></View>
+        <View style={styles.signin}></View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -18,89 +30,36 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
-  image: {
+  background: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  content: {
+    alignItems: "center",
     width: "100%",
-    height: "100%",
+    position: "absolute",
+    top: 60,
+  },
+  bottomContainer: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+  },
+  signup: {
+    backgroundColor: "tomato",
+    height: 50,
+    width: "100%",
+  },
+  signin: {
+    backgroundColor: "dodgerblue",
+    height: 50,
+    width: "100%",
+  },
+  logoImage: {
+    height: 120,
+    width: 120,
+  },
+  welcomeText: {
+    marginTop: 20,
   },
 });
-
-//  return (
-//    <ParallaxScrollView
-//      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-//      headerImage={
-//        <Image
-//          source={require("@/assets/images/partial-react-logo.png")}
-//          style={styles.reactLogo}
-//        />
-//      }
-//    >
-//      <ThemedView style={styles.titleContainer}>
-//        <ThemedText type="title">Welcome!</ThemedText>
-//        <HelloWave />
-//      </ThemedView>
-//      <ThemedView style={styles.stepContainer}>
-//        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-//        <ThemedText>
-//          Edit{" "}
-//          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to
-//          see changes. Press{" "}
-//          <ThemedText type="defaultSemiBold">
-//            {Platform.select({
-//              ios: "cmd + d",
-//              android: "cmd + m",
-//              web: "F12",
-//            })}
-//          </ThemedText>{" "}
-//          to open developer tools.
-//        </ThemedText>
-//      </ThemedView>
-//      <ThemedView style={styles.stepContainer}>
-//        <Link href="/modal">
-//          <Link.Trigger>
-//            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-//          </Link.Trigger>
-//          <Link.Preview />
-//          <Link.Menu>
-//            <Link.MenuAction
-//              title="Action"
-//              icon="cube"
-//              onPress={() => alert("Action pressed")}
-//            />
-//            <Link.MenuAction
-//              title="Share"
-//              icon="square.and.arrow.up"
-//              onPress={() => alert("Share pressed")}
-//            />
-//            <Link.Menu title="More" icon="ellipsis">
-//              <Link.MenuAction
-//                title="Delete"
-//                icon="trash"
-//                destructive
-//                onPress={() => alert("Delete pressed")}
-//              />
-//            </Link.Menu>
-//          </Link.Menu>
-//        </Link>
-
-//        <ThemedText>
-//          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-//        </ThemedText>
-//      </ThemedView>
-//      <ThemedView style={styles.stepContainer}>
-//        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-//        <ThemedText>
-//          {`When you're ready, run `}
-//          <ThemedText type="defaultSemiBold">
-//            npm run reset-project
-//          </ThemedText>{" "}
-//          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-//          directory. This will move the current{" "}
-//          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-//          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-//        </ThemedText>
-//      </ThemedView>
-//    </ParallaxScrollView>
-//  );
